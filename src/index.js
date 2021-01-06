b887bd39c75134c07e5bf8536eba8c28f402007f
anberjamz <anberjam@yahoo.com>
2021-01-06 10:59:22 -0500

    progress in like button and fav button

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const divOther = document.querySelector('div#Other.tabcontent')
 function renderAllDesserts(dessertArray) {
     dessertArray.forEach(dessertObj => {
       renderOneDessert(dessertObj)
+     
     })  
   }
 
@@ -15,13 +16,20 @@ function renderOneDessert(dessertObj) {
     const div = document.createElement("div")
 
     
+
+    
     div.innerHTML = `
     <h2>${dessertObj.name}</h2>
     <img src="${dessertObj.picture}" class="dessert-picture" />
     
-    <button class="like-btn">Like ♡ </button>
+    <button class="like-btn" id=${dessertObj.id}>Like ♡ </button>
     <button class="add-btn" id = ${dessertObj.id}> ADD TO FAVORITES </button>
     `
+
+
+    
+
+
 
     if (dessertObj.category=="Cake")
     {divCake.append(div)}
@@ -74,7 +82,7 @@ function renderOneDessert(dessertObj) {
       dessertCollection.append(divOther)
 
 
-      const likeButton = document.querySelector
+      const likeButton = document.querySelector('button.like-btn')
 
 
 
@@ -85,30 +93,54 @@ const favButton = document.querySelector('button.add-btn')
 
 
 dessertCollection.addEventListener("click", function(event) {
-  if (event.target.matches(".add-btn")) {
+  if (event.target.matches(".like-btn")) {
     const dessertId = event.target.id
+    event.target.style.background = '#FF3333';
+    
+    //fetch dessert data and change likes to 1. also change color of like
+
+    configObj = {method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({"likes": 1
+    })}
+
+
+    
+
+    fetch(`http://localhost:3000/desserts/${dessertId}`, configObj)
+    .then(r => r.json())
+    .then(console.log)
+    
+    
+   
+
+  }})
+
+
 
     //create a new favorite object
 
-    const newFav = {
-      dessert_id: dessertId
-    }
+    // const newFav = {
+    //   dessert_id: dessertId
+    // }
     
-    fetch(`http://localhost:3000/favorites`, {
-      method: 'POST',
-      headers: { "content-type": "application/json"}, 
-      body: JSON.stringify({newFav})
-    })
-    .then((res) => res.json())
-    .then((updatedFavs) => {console.log(updatedFavs)})
-    //create a method that adds fav to fav list
+    // fetch(`http://localhost:3000/favorites`, {
+    //   method: 'POST',
+    //   headers: { "content-type": "application/json"}, 
+    //   body: JSON.stringify({newFav})
+    // })
+    // .then((res) => res.json())
+    // .then((updatedFavs) => {console.log(updatedFavs)})
+    // //create a method that adds fav to fav list
 
 
     
-   }
+   
 
 
 
 
 
-})
+
