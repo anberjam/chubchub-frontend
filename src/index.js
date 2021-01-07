a4876370af8c0a4ce91e37916b23f753bd0ca95b
anberjamz <anberjam@yahoo.com>
2021-01-07 15:09:20 -0500

    most updated

--- src/index.js
+++ src/index.js
@@ -11,22 +11,69 @@ function renderAllDesserts(dessertArray) {
     })  
   }
 
-  
+  function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+  } 
+
+
 function renderOneDessert(dessertObj) {
     const div = document.createElement("div")
 
-    
+    const img = document.createElement('img')
+    img.src = dessertObj.picture
+    img.className = 'dessert-picture'
+    img.id = dessertObj.id
+    img.addEventListener('click', function(event){
+      const editDiv = document.createElement('div')
+      
+      editDiv.innerHTML =`
+          <form class='edit-dessert-form' dessertId='edit-${dessertObj.id}'>
+            <h3>Edit this Dessert</h3>
+            <select name='category' class='update-category-${dessertObj.id}'>
+              <option value="Pie">Pie</option>
+              <option value="Cake">Cake</option>
+              <option value="Tart">Tart</option>
+              <option value="Other">Other</option>
+              <br />
+            </select>
+            <input type="text" name="name" value="" placeholder="${dessertObj.name}" class="update-dessert-name-${dessertObj.id}" />
+            <br />
+            <input type="text" name="picture" value="" placeholder="${dessertObj.picture}" class="update-dessert-picture-${dessertObj.id}" />
+            <br />
+            <button type='button' dessertid='${dessertObj.id}' other='${dessertObj.id}' class='update-dessert-button'> Update this dessert</button>
+            <button type='button' id='delete-${dessertObj.id}' dessertId='${dessertObj.id}' class='delete-dessert-button'> Delete this dessert</button>
+          </form>`
+      div.append(editDiv)
+      const deleteButton = document.querySelector(`#delete-${dessertObj.id}`)
+      
+      deleteButton.addEventListener("click", function(event){
+        // console.log('clicked')
+        // console.log(event.target.id)
+        // console.log(dessertObj.id)
+        
+        fetch(`http://localhost:3000/desserts/${dessertObj.id}`, { method: "DELETE" })
+        .then(r => r.json())
+        .then(console.log)
+      })
+          
+   
+    })
 
     
     div.innerHTML = `
-    <h2>${dessertObj.name}</h2>
-    <img src="${dessertObj.picture}" class="dessert-picture"/>
+    <h2 id='name-${dessertObj.id}'>${dessertObj.name}</h2>
     
     <button class="like-btn" id=${dessertObj.id} style= "background: rgb(51,255,211)">Like ♡ </button>
     <button class="add-btn" id = ${dessertObj.id}> ADD TO FAVORITES </button>
     `
 
-
+    for (i = 0; i < div.children.length; i++) {
+      const node = div.children[i]; 
+      if (node.id === `name-${dessertObj.id}`){
+       insertAfter(img, node)
+        break
+      }
+    }
     
 
 
@@ -186,56 +233,82 @@ favListSection.addEventListener("click", function(event){
     closestA.remove()
 
 
-    fetch(`http://localhost:3000/favorites/${specificFavId}`, { method: "DELETE" })
-
-        
-
-        
+    fetch(`http://localhost:3000/favorites/${specificFavId}`, { method: "DELETE" })   
 
   }
 
-
-
 })
 
-
-
-
-
-
-
-//render one fav
-//render all fav
-//get fetch favorites 
-
-
-
-
-
-// function renderAllFavorites(favoriteArray){
-//   favoriteArray.forEach(favoriteObj => {
-//     renderOneFavorite(favoriteObj) 
-   
-//   })  
-// }
-
-
-
-// function renderOneFavorite(favoriteObj){
-
-// }
-
-
-
-// fetch("http://localhost:3000/favorites")
-// .then(r => r.json())
-// .then(favoriteArray => {
-
-//   renderAllFavorites(favoriteArray)
-// })
-
+      //make image clickable, when click, get edit form
+      //fills the form
+      //edit form submit, fetch data and patch it
+      //after submit, closes the form
+      dessertCollection.addEventListener("click", function(event) {
+        if (event.target.matches(".update-dessert-button")) {
+          const dessertId = event.target.attributes.dessertid.value;
+          // console.log({ dessertId, event: event.target })
     
-   
+          const updateDessertForm = document.querySelector(`edit-${dessertId}`);
+    
+          const updateNameInput = document.querySelector(`.update-dessert-name-${dessertId}`)
+          const updatePicture=document.querySelector(`.update-dessert-picture-${dessertId}`)
+          const updateCategory=document.querySelector(`.update-category-${dessertId}`)
+    
+          // const updateName = updateNameInput.value
+    
+          console.log(updateCategory)
+          const updateDessertObj = {
+          "name": updateNameInput.value,
+          "picture": updatePicture.value,
+          "category": updateCategory.value,
+            "likes": 0
+          }
+    
+          configObj = {method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updateDessertObj) }
+    
+          fetch(`http://localhost:3000/desserts/${dessertId}`, configObj)
+          .then(r => r.json())
+          .then(console.log)
+        }})
+
+    //new dessert form
+    const newDessertForm = document.querySelector('.new-dessert-form')
+    const dessertFormDiv = document.querySelector('.dessert-form-div')
+    const url = 'http://localhost:3000/desserts'
+    
+    
+    
+    newDessertForm.addEventListener('submit', function(event){
+      event.preventDefault()
+    
+      const newDessertObj = {
+        "name": event.target.name.value,
+        "picture": event.target.picture.value,
+          "category": event.target.category.value,
+          "likes": 0
+      }
+    
+      const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(newDessertObj) }
+    
+    
+        fetch(url, config)
+        .then(r => r.json())
+        .then(newDessertObject => {
+    
+          renderOneDessert(newDessertObject)
+        })
+      })
+    
 
 
 
